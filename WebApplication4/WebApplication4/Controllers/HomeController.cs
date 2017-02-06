8be461f92e047bbaf014e107743c9b2f5c5dfaa9
unknown <@NB0042sl.bdo.ru>
2017-02-06 17:53:43 +0300

    Application to rep

---
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.IO;
+using WebApplication4.Models;
+namespace WebApplication4.Controllers { 
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult Edit()
+        {
+            if (System.IO.File.Exists("C:/index.txt"))
+            {
+                ViewBag.textFile = System.IO.File.ReadAllText("C:/index.txt");
+            }
+            return View();
+       } 
+         [HttpPost]
+        public ViewResult Edit(FileModel fileModel)
+        {
+            string path = "C:/index.txt";
+            System.IO.File.WriteAllText(path, fileModel.text);
+            return View("Index", fileModel);
+        }
+     }
+}
+ 
---
+++ WebApplication4/WebApplication4/Models/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class FileModel
+    {
+        public string text { get; set; }
+        public string path { get; set; }
+    }
+}
